2152d48f014ceeebc04082b2baf301055d9fb329
sanye891 <2388292920@qq.com>
2025-09-20 13:39:14 +0800

    背景修改

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body className="font-sans antialiased">
         <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
       </body>
